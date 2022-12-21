16642cef1cc4c661f84f50dfef0ea7ae333e6338
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-12-21 14:58:54 +0500

    dashboard

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Navbar from './components/navbar/Navbar';
+
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  return (
+    <div className="container">
+      < Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <h1>React Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
